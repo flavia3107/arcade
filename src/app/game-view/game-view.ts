303cb894ffe2d77b0b8d69ecbf1c319577b1a353
Flavia Kosti <flaviakosti@gmail.com>
2025-12-10 18:25:57 -0500

    Arcade: Setup P11

--- src/app/game-view/game-view.ts
+++ src/app/game-view/game-view.ts
@@ -1,18 +1,22 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { map } from 'rxjs';
 import { SafeUrlPipe } from '../../shared/pipes/safeUrl';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { GAMES } from '../../shared/configs/models';
 
 @Component({
   selector: 'app-game-view',
-  imports: [SafeUrlPipe, AsyncPipe, MatButtonModule],
+  imports: [SafeUrlPipe, AsyncPipe, MatButtonModule, MatMenuModule, RouterModule],
   templateUrl: './game-view.html',
   styleUrl: './game-view.scss',
 })
 export class GameView {
   private _route = inject(ActivatedRoute);
+  public games = GAMES;
+
   public gameUrl$ = this._route.paramMap.pipe(
     map(params => {
       const slug = params.get('slug');
