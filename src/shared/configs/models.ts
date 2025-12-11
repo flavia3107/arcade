93b420e937cebab47f1c62cb2bb3c985ed916895
Flavia Kosti <flaviakosti@gmail.com>
2025-12-11 18:27:15 -0500

    Arcade: Setup P13

--- src/shared/configs/models.ts
+++ src/shared/configs/models.ts
@@ -1,9 +1,9 @@
 export const GAMES = [
-	{ name: 'Timer', url: 'timer-game' },
-	{ name: 'Tic-Tac-Toe', url: 'tic-tac-toe' },
-	{ name: 'Gorilla', url: 'freecodecamp-gorilla-game' },
-	{ name: 'Snake', url: 'freecodecamp-snake-game' },
-	{ name: 'Hangman', url: 'freecodecamp-melting-snowman' },
-	{ name: 'Quiz', url: 'quiz' },
-	{ name: 'Sonic', url: 'freecodecamp-sonic-game' }
+	{ name: 'Timer', url: 'timer-game', img: '/gorilla.png' },
+	{ name: 'Tic-Tac-Toe', url: 'tic-tac-toe', img: '/gorilla.png' },
+	{ name: 'Gorilla', url: 'freecodecamp-gorilla-game', img: '/gorilla.png' },
+	{ name: 'Snake', url: 'freecodecamp-snake-game', img: '/gorilla.png' },
+	{ name: 'Hangman', url: 'freecodecamp-melting-snowman', img: '/gorilla.png' },
+	{ name: 'Quiz', url: 'quiz', img: '/gorilla.png' },
+	{ name: 'Sonic', url: 'freecodecamp-sonic-game', img: '/gorilla.png' }
 ];
