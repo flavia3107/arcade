6911d1b9e2a22ad1a2269a64e05ad21e9d0bd232
Flavia Kosti <flaviakosti@gmail.com>
2025-12-12 20:38:17 -0500

    Arcade: Setup P21

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: ':slug',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
