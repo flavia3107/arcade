217b3c560593f259483f27f90a2c2d02472728da
Flavia Kosti <flaviakosti@gmail.com>
2025-12-16 21:59:13 -0500

    Arcade: Setup P26

--- src/shared/configs/models.ts
+++ src/shared/configs/models.ts
@@ -3,42 +3,49 @@ export const GAMES = [
 		name: 'Timer',
 		url: 'timer-game',
 		img: '/timer.png',
-		cover: '/game-backgrounds/bg-1.jpg'
+		cover: '/game-backgrounds/bg-1.jpg',
+		gradient: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)'
 	},
 	{
 		name: 'Tic-Tac-Toe',
 		url: 'tic-tac-toe',
 		img: '/tic-tac-toe.png',
-		cover: '/game-backgrounds/bg-2.jpg'
+		cover: '/game-backgrounds/bg-2.jpg',
+		gradient: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)'
 	},
 	{
 		name: 'Gorilla',
 		url: 'freecodecamp-gorilla-game',
 		img: '/gorilla.png',
-		cover: '/game-backgrounds/bg-3.jpg'
+		cover: '/game-backgrounds/bg-3.jpg',
+		gradient: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)'
 	},
 	{
 		name: 'Snake',
 		url: 'freecodecamp-snake-game',
 		img: '/snake.png',
-		cover: '/game-backgrounds/bg-4.png'
+		cover: '/game-backgrounds/bg-4.png',
+		gradient: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)'
 	},
 	{
 		name: 'Hangman',
 		url: 'freecodecamp-melting-snowman',
 		img: '/hangman.png',
-		cover: '/game-backgrounds/bg-5.jpg'
+		cover: '/game-backgrounds/bg-5.jpg',
+		gradient: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)'
 	},
 	{
 		name: 'Quiz',
 		url: 'quiz',
 		img: '/quiz.png',
-		cover: '/game-backgrounds/bg-6.jpg'
+		cover: '/game-backgrounds/bg-6.jpg',
+		gradient: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)'
 	},
 	{
 		name: 'Sonic',
 		url: 'freecodecamp-sonic-game',
 		img: '/sonic.png',
-		cover: '/game-backgrounds/bg-7.jpg'
+		cover: '/game-backgrounds/bg-7.jpg',
+		gradient: 'linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)'
 	}
 ];
