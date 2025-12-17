cad52635d82289cf42d805401d0e14a33f79aa8c
Flavia Kosti <flaviakosti@gmail.com>
2025-12-16 21:51:24 -0500

    Arcade: Setup P25

--- src/shared/configs/models.ts
+++ src/shared/configs/models.ts
@@ -1,9 +1,44 @@
 export const GAMES = [
-	{ name: 'Timer', url: 'timer-game', img: '/timer.png' },
-	{ name: 'Tic-Tac-Toe', url: 'tic-tac-toe', img: '/tic-tac-toe.png' },
-	{ name: 'Gorilla', url: 'freecodecamp-gorilla-game', img: '/gorilla.png' },
-	{ name: 'Snake', url: 'freecodecamp-snake-game', img: '/snake.png' },
-	{ name: 'Hangman', url: 'freecodecamp-melting-snowman', img: '/hangman.png' },
-	{ name: 'Quiz', url: 'quiz', img: '/quiz.png' },
-	{ name: 'Sonic', url: 'freecodecamp-sonic-game', img: '/sonic.png' }
+	{
+		name: 'Timer',
+		url: 'timer-game',
+		img: '/timer.png',
+		cover: '/game-backgrounds/bg-1.jpg'
+	},
+	{
+		name: 'Tic-Tac-Toe',
+		url: 'tic-tac-toe',
+		img: '/tic-tac-toe.png',
+		cover: '/game-backgrounds/bg-2.jpg'
+	},
+	{
+		name: 'Gorilla',
+		url: 'freecodecamp-gorilla-game',
+		img: '/gorilla.png',
+		cover: '/game-backgrounds/bg-3.jpg'
+	},
+	{
+		name: 'Snake',
+		url: 'freecodecamp-snake-game',
+		img: '/snake.png',
+		cover: '/game-backgrounds/bg-4.png'
+	},
+	{
+		name: 'Hangman',
+		url: 'freecodecamp-melting-snowman',
+		img: '/hangman.png',
+		cover: '/game-backgrounds/bg-5.jpg'
+	},
+	{
+		name: 'Quiz',
+		url: 'quiz',
+		img: '/quiz.png',
+		cover: '/game-backgrounds/bg-6.jpg'
+	},
+	{
+		name: 'Sonic',
+		url: 'freecodecamp-sonic-game',
+		img: '/sonic.png',
+		cover: '/game-backgrounds/bg-7.jpg'
+	}
 ];
