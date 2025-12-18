75b41066f34589c8806c6f65ad6c83eee4882850
Flavia Kosti <flaviakosti@gmail.com>
2025-12-17 21:54:48 -0500

    Arcade: Setup P28

--- src/shared/configs/models.ts
+++ src/shared/configs/models.ts
@@ -4,48 +4,48 @@ export const GAMES = [
 		url: 'timer-game',
 		img: '/timer.png',
 		cover: '/game-backgrounds/bg-1.jpg',
-		gradient: 'linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%)'
+		gradient: 'linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%)'
 	},
 	{
 		name: 'Tic-Tac-Toe',
 		url: 'tic-tac-toe',
 		img: '/tic-tac-toe.png',
 		cover: '/game-backgrounds/bg-2.jpg',
-		gradient: 'linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%)'
+		gradient: 'linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%)'
 	},
 	{
 		name: 'Gorilla',
 		url: 'freecodecamp-gorilla-game',
 		img: '/gorilla.png',
 		cover: '/game-backgrounds/bg-3.jpg',
-		gradient: 'linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%)'
+		gradient: 'linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%)'
 	},
 	{
 		name: 'Snake',
 		url: 'freecodecamp-snake-game',
 		img: '/snake.png',
 		cover: '/game-backgrounds/bg-4.png',
-		gradient: 'linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%)'
+		gradient: 'linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%)'
 	},
 	{
 		name: 'Hangman',
 		url: 'freecodecamp-melting-snowman',
 		img: '/hangman.png',
 		cover: '/game-backgrounds/bg-5.jpg',
-		gradient: 'linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%)'
+		gradient: 'linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%)'
 	},
 	{
 		name: 'Quiz',
 		url: 'quiz',
 		img: '/quiz.png',
 		cover: '/game-backgrounds/bg-6.jpg',
-		gradient: 'linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%)'
+		gradient: 'linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%)'
 	},
 	{
 		name: 'Sonic',
 		url: 'freecodecamp-sonic-game',
 		img: '/sonic.png',
 		cover: '/game-backgrounds/bg-7.jpg',
-		gradient: 'linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%)'
+		gradient: 'linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%)'
 	}
 ];
